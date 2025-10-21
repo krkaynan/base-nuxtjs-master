5273fb246b1b049d931ebab3a998ef8c75e142bb
Kaynan Raikkonen <raikkonenkaynan@gmail.com>
2025-10-21 14:15:38 -0300

    add componets/global/nav

---
+++ components/global/nav/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav></nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+nav {
+  display: flex;
+}
+/* Responsivo */
+@media screen and (max-width: 1140px) {
+}
+</style>
