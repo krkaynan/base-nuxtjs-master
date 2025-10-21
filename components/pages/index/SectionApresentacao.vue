831e806648024904c429e1a9e3fb7f671730c3ae
Kaynan Raikkonen <raikkonenkaynan@gmail.com>
2025-10-21 14:14:09 -0300

    add componets/pages

---
+++ components/pages/index/SectionApresentacao.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="apresentacao"></section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+section.apresentacao {
+  display: flex;
+}
+/* Responsivo */
+@media screen and (max-width: 1140px) {
+}
+</style>
