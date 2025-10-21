6808a204119cf7baac321ba94f0bf2126bb9280e
Kaynan Raikkonen <raikkonenkaynan@gmail.com>
2025-10-21 14:14:50 -0300

    add componets/global/footer

---
+++ components/global/footer/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer></footer>
+</template>
+
+<script setup></script>
+
+<style scoped>
+footer {
+  display: flex;
+}
+/* Responsivo */
+@media screen and (max-width: 1140px) {
+}
+</style>
